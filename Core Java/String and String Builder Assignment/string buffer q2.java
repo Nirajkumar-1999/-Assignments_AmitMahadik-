6c1d7dd42f7218cb04f7cadf3f9aa96946f43a7a
Nirajkumar-1999 <78890787+Nirajkumar-1999@users.noreply.github.com>
2022-02-05 13:53:37 +0530

    Create string buffer q2.java

---
+++ Core Java/String and String Builder Assignment/string buffer q2.java
@@ -0,0 +1,14 @@
+public class Niraj {
+
+	public static void main(String[] args) {
+	
+
+		StringBuffer Sbf = new StringBuffer("It is used to _ at the "
+				+ "specified index position");
+		StringBuffer Sbf1 = new StringBuffer("insert text");
+		Sbf.delete(14, 15);
+		Sbf.insert(14, s2);
+		System.out.println(Sbf);
+	}
+
+}
