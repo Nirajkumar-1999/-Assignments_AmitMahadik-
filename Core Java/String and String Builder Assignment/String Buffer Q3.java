b2fc2977d65e9a500942178d5aa57b7b406dc252
Nirajkumar-1999 <78890787+Nirajkumar-1999@users.noreply.github.com>
2022-02-05 13:56:00 +0530

    Create String Buffer Q3.java

---
+++ Core Java/String and String Builder Assignment/String Buffer Q3.java
@@ -0,0 +1,12 @@
+public class Niraj {
+
+	public static void main(String[] args) {
+		
+
+		StringBuffer Sbf1 = new StringBuffer("This method returns "
+				+ "the reversed object on which it was called");
+		Sbf1.reverse();
+		System.out.println(sbf1);
+	}
+
+}
