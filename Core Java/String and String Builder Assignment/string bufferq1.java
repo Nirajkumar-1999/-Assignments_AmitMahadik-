24accb5f917a0be1a31271aefa401c360bd150b4
Nirajkumar-1999 <78890787+Nirajkumar-1999@users.noreply.github.com>
2022-02-05 13:50:15 +0530

    Create string bufferq1.java

---
+++ Core Java/String and String Builder Assignment/string bufferq1.java
@@ -0,0 +1,16 @@
+public class Niraj {
+
+	public static void main(String[] args) {
+		
+
+		StringBuffer sbf1 = new StringBuffer("StringBuffer ");
+		StringBuffer sbf2 = new StringBuffer("is a peer class of String ");
+		StringBuffer sbf3 = new StringBuffer("that provides much of ");
+		StringBuffer sbf4= new StringBuffer("the functionality of strings.");
+		s1.append(s2);
+	  s3.append(s4);
+		s1.append(s3);
+		System.out.println(s1);
+	}
+
+}
