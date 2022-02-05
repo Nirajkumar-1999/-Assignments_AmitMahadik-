476ed7a6762437607ac21ed5908634a76f0e80a2
Nirajkumar-1999 <78890787+Nirajkumar-1999@users.noreply.github.com>
2022-02-05 14:04:56 +0530

    Create String Builder Q2.java

---
+++ Core Java/String and String Builder Assignment/String Builder Q2.java
@@ -0,0 +1,14 @@
+public class StringBuilder1 {
+
+	public static void main(String[] args) {
+	
+
+		StringBuilder Sbf = new StringBuilder("It is used to _ at the "
+				+ "specified index position");
+		StringBuilder Sbf1 = new StringBuilder("insert text");
+		Sbf.delete(14, 15);
+		Sbf.insert(14, s2);
+		System.out.println(Sbf);
+	}
+
+}
