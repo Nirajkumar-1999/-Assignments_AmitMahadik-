2f3fd32fbb366168ed75d61e541ff72cb861422b
Nirajkumar-1999 <78890787+Nirajkumar-1999@users.noreply.github.com>
2022-02-05 14:03:40 +0530

    Create StringBuilder Q3.java

---
+++ Core Java/String and String Builder Assignment/StringBuilder Q3.java
@@ -0,0 +1,12 @@
+public class StringBuilder1 {
+
+	public static void main(String[] args) {
+		
+
+		StringBuilder Sbf1 = new StringBuilder("This method returns "
+				+ "the reversed object on which it was called");
+		Sbf1.reverse();
+		System.out.println(sbf1);
+	}
+
+}
